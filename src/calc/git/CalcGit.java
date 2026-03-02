2bb4d6b87f0b72179cfa539b6f465dae7c639959
ST10523424 <ST10523424@myemeris.edu.za>
2026-03-02 08:26:24 +0200

    initialize commit
    I don't know

--- src/calc/git/CalcGit.java
+++ src/calc/git/CalcGit.java
@@ -15,8 +15,8 @@ public class CalcGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
+        // TODO code application logic here 
+       //IDFK ._.
     }
     
 }
